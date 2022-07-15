a72c408b91aeb9e5fa365a2d357c49ab82502181
Aaron Challinor <aaronchallinor@gmail.com>
2022-07-16 00:41:40 +0100

    Add input, resize grid based on input.

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const container = document.getElementById("container"); //Telling the JS where our container is.
-let pixelCount = 100; //determines how many times the for loop runs
+let pixelCount = 0; //determines how many times the for loop runs
+let submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click",() => {calcGrid();})
 
 
+
+function calcGrid(){
+    let oneSide = document.getElementById("sizeInput").value
+    pixelCount = oneSide*oneSide;
+    container.style.maxWidth = oneSide * 20 + 'px';
+    generateGrid()
+    
+
+
+
+}
+
+
+function generateGrid(){
+    container.innerHTML = "";
 for (let i = 0; i < pixelCount;i++){
     let pixel = document.createElement("div"); //create a div
     pixel.id = `pixel${i}`; //assign it a unique id
     pixel.className = "pixel"; //give it the pixel class to pick up the css styling
     //pixel.innerText = `${i}`; //stick it's loop number in there (just a test)
     container.appendChild(pixel); //append it to the container
-}
+
+
+}}
+
 
 
 //It's going to be something about resizing the container based on the amount of boxes generated.
